8613fc6331737c59739876f483c1fcfa0a922b00
Marcos <mvinicius.oliveira04@gmail.com>
2021-04-29 21:20:30 -0300

    Calculadora de idade

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Calculadora_de_idade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int anoIdade, anoAtual, idade, semanas;
+
+            Console.WriteLine("Calculadora de idade iniciada\n");
+
+            Console.Write("Digite o seu ano de nascimento: ");
+            anoIdade = int.Parse(Console.ReadLine());
+            anoAtual = System.DateTime.Now.Year;
+
+            idade = anoAtual - anoIdade;
+            semanas = idade * 52;
+
+            Console.WriteLine("\nSua idade atual em anos é de " + idade + " anos.");
+            Console.WriteLine($"Sua idade atual em semanas é de {semanas} semanas.");
+
+            Console.WriteLine("\nPrograma finalizado.");
+        }
+    }
+}
